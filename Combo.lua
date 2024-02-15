4bc68c24fc141a637e70ac2c173421c08461da4d
CDZGold <160167927+CDZGold@users.noreply.github.com>
2024-02-15 16:26:27 -0300

    Update Combo.lua

--- Combo.lua
+++ Combo.lua
@@ -37,3 +37,21 @@ onKeyPress(function(keys)
         say(storage.sspell)
     end
 end)
+
+local revidar = false
+addSwitch("revidar", "revidar", function(widget)
+    revidar = not revidar
+    widget:setOn(revidar)
+end)
+
+onTextMessage(function(mode, text)
+  if revidar == true and not g_game.getAttackingCreature() and string.find(text, "You lose") then
+    local targetName = string.sub(text, string.find(text, " [^ ]*$") + 1, #text - 1)
+  local target = getPlayerByName(targetName)
+if target then
+  g_game.attack(target)
+  CaveBot.setOff()
+  TargetBot.setOff()
+end
+  end
+end)
